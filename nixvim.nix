c35370ef6ba6e6b2e54b7adb22515a7f421259c2
auipga <auipga@gmail.com>
2025-03-10 18:31:50 +0100

    colorschemes.catppuccin

--- nixvim.nix
+++ nixvim.nix
@@ -45,13 +45,14 @@
     #
     # If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     colorschemes = {
-      # https://nix-community.github.io/nixvim/colorschemes/tokyonight/index.html
-      tokyonight = {
+      # https://nix-community.github.io/nixvim/colorschemes/catppuccin/index.html
+      catppuccin = {
         enable = true;
         settings = {
-          # Like many other themes, this one has different styles, and you could load
-          # any other, such as 'storm', 'moon', or 'day'.
-          style = "night";
+          style = "catppuccin-mocha";
+          styles = {
+            comments = []; # fixme: comments are still italic
+          };
         };
       };
     };
