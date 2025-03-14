1bc39ebcae313800325b619709853e61e1bc349a
auipga <auipga@gmail.com>
2025-03-14 13:46:10 +0100

    configure undotree plugin

--- plugins/custom/plugins/undotree.nix
+++ plugins/custom/plugins/undotree.nix
@@ -1,6 +1,12 @@
 {
   programs.nixvim = {
     plugins.undotree.enable = true;
+    plugins.undotree = {
+      settings = {
+        ShortIndicators = true; # default: 0
+        WindowLayout = 3; # default: 1
+      };
+    };
 
     # https://nix-community.github.io/nixvim/keymaps/index.html
     keymaps = [
