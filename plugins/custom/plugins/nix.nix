c89c0e9f512676c9bf35a98ce9ad842e65b7d97c
auipga <auipga@gmail.com>
2025-03-18 16:10:47 +0100

    add nil_ls lsp server for Nix

--- plugins/custom/plugins/nix.nix
+++ plugins/custom/plugins/nix.nix
@@ -7,5 +7,9 @@
     # https://github.com/LnL7/vim-nix/
     # https://nix-community.github.io/nixvim/plugins/nix.html
     plugins.nix.enable = true;
+
+    # https://github.com/oxalica/nil
+    # https://nix-community.github.io/nixvim/plugins/lsp/servers/nil_ls/index.html
+    plugins.lsp.servers.nil_ls.enable = true;
   };
 }
