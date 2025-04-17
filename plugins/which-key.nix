8431734e722e097bb3c46559aa33105fa8099acf
auipga <auipga@gmail.com>
2025-04-17 16:13:03 +0200

    conditionally set plugins.which-key.settings.icons.keys by have_nerd_font

--- plugins/which-key.nix
+++ plugins/which-key.nix
@@ -1,3 +1,4 @@
+{ config, ... }:
 {
   programs.nixvim = {
     # Useful plugin to show you pending keybinds.
@@ -7,6 +8,73 @@
 
       # Document existing key chains
       settings = {
+        icons = {
+          keys = if config.programs.nixvim.globals.have_nerd_font then
+            # default
+            {
+              BS = "󰁮";
+              C = "󰘴 ";
+              CR = "󰌑 ";
+              D = "󰘳 ";
+              Down = " ";
+              Esc = "󱊷 ";
+              F1 = "󱊫";
+              F10 = "󱊴";
+              F11 = "󱊵";
+              F12 = "󱊶";
+              F2 = "󱊬";
+              F3 = "󱊭";
+              F4 = "󱊮";
+              F5 = "󱊯";
+              F6 = "󱊰";
+              F7 = "󱊱";
+              F8 = "󱊲";
+              F9 = "󱊳";
+              Left = " ";
+              M = "󰘵 ";
+              NL = "󰌑 ";
+              Right = " ";
+              S = "󰘶 ";
+              ScrollWheelDown = "󱕐 ";
+              ScrollWheelUp = "󱕑 ";
+              Space = "󱁐 ";
+              Tab = "󰌒 ";
+              Up = " ";
+            }
+            else
+            # kickstart
+            {
+              Up = "<Up> ";
+              Down = "<Down> ";
+              Left = "<Left> ";
+              Right = "<Right> ";
+              C = "<C-…> ";
+              M = "<M-…> ";
+              D = "<D-…> ";
+              S = "<S-…> ";
+              CR = "<CR> ";
+              Esc = "<Esc> ";
+              ScrollWheelDown = "<ScrollWheelDown> ";
+              ScrollWheelUp = "<ScrollWheelUp> ";
+              NL = "<NL> ";
+              BS = "<BS> ";
+              Space = "<Space> ";
+              Tab = "<Tab> ";
+              F1 = "<F1>";
+              F2 = "<F2>";
+              F3 = "<F3>";
+              F4 = "<F4>";
+              F5 = "<F5>";
+              F6 = "<F6>";
+              F7 = "<F7>";
+              F8 = "<F8>";
+              F9 = "<F9>";
+              F10 = "<F10>";
+              F11 = "<F11>";
+              F12 = "<F12>";
+            };
+        };
+
         spec = [
           {
             __unkeyed-1 = "<leader>c";
