c99be2a74b75fe0635ee80efd23928ab54f7af82
auipga <auipga@gmail.com>
2025-04-19 05:58:38 +0200

    use 'map' functions for keymaps in gitsigns.nix

--- plugins/gitsigns.nix
+++ plugins/gitsigns.nix
@@ -1,3 +1,9 @@
+let
+  map = import ../lib/mkKeymap.nix { };
+  mapP = import ../lib/mkKeymap.nix { prefix = "git "; };
+  mapPR = import ../lib/mkKeymap.nix { prefix = "git "; raw = true; };
+  mapR = import ../lib/mkKeymap.nix { raw = true; };
+in
 {
   programs.nixvim = {
     # Adds git related signs to the gutter, as well as utilities for managing changes
@@ -19,10 +25,7 @@
 
     keymaps = [
       # Navigation
-      {
-        mode = "n";
-        key = "]c";
-        action.__raw = ''
+      (mapR [ "]c" ''
           function()
             if vim.wo.diff then
               vim.cmd.normal { ']c', bang = true }
@@ -30,15 +33,8 @@
               require('gitsigns').nav_hunk 'next'
             end
           end
-        '';
-        options = {
-          desc = "Jump to next git [c]hange";
-        };
-      }
-      {
-        mode = "n";
-        key = "[c";
-        action.__raw = ''
+        '' "Jump to next git [c]hange" ])
+      (mapR [ "[c" ''
           function()
             if vim.wo.diff then
               vim.cmd.normal { '[c', bang = true }
@@ -46,262 +42,57 @@
               require('gitsigns').nav_hunk 'prev'
             end
           end
-        '';
-        options = {
-          desc = "Jump to previous git [c]hange";
-        };
-      }
+        '' "Jump to previous git [c]hange" ])
 
       # Actions
       # visual mode
-      {
-        mode = "v";
-        key = "<leader>hs";
-        action.__raw = ''
+      (mapPR [ "<leader>hs" ''
           function()
             require('gitsigns').stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
           end
-        '';
-        options = {
-          desc = "git [s]tage hunk";
-        };
-      }
-      {
-        mode = "v";
-        key = "<leader>hr";
-        action.__raw = ''
+        '' "[s]tage hunk" "v" ])
+      (mapPR [ "<leader>hr" ''
           function()
             require('gitsigns').reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
           end
-        '';
-        options = {
-          desc = "git [r]eset hunk";
-        };
-      }
+        '' "[r]eset hunk" "v" ])
+
       # normal mode
-      {
-        mode = "n";
-        key = "<leader>hs";
-        action.__raw = ''
-          function()
-            require('gitsigns').stage_hunk()
-          end
-        '';
-        options = {
-          desc = "git [s]tage hunk";
-        };
-      }
-      {
-        mode = "n";
-        key = "<leader>hr";
-        action.__raw = ''
-          function()
-            require('gitsigns').reset_hunk()
-          end
-        '';
-        options = {
-          desc = "git [r]eset hunk";
-        };
-      }
-      {
-        mode = "n";
-        key = "<leader>hS";
-        action.__raw = ''
-          function()
-            require('gitsigns').stage_buffer()
-          end
-        '';
-        options = {
-          desc = "git [S]tage buffer";
-        };
-      }
-      {
-        mode = "n";
-        key = "<leader>hu";
-        # `undo_stage_hunk` is deprecated. See https://github.com/nvim-lua/kickstart.nvim/issues/1319
-        #  Replacement `stage_hunk` was accidentally merged. See https://github.com/nvim-lua/kickstart.nvim/pull/1321#issuecomment-2664265962
-        action.__raw = ''
-          function()
-            require('gitsigns').undo_stage_hunk()
-          end
-        '';
-        options = {
-          desc = "git [u]ndo stage hunk";
-        };
-      }
-      {
-        mode = "n";
-        key = "<leader>hR";
-        action.__raw = ''
-          function()
-            require('gitsigns').reset_buffer()
-          end
-        '';
-        options = {
-          desc = "git [R]eset buffer";
-        };
-      }
-      {
-        mode = "n";
-        key = "<leader>hp";
-        action.__raw = ''
-          function()
-            require('gitsigns').preview_hunk()
-          end
-        '';
-        options = {
-          desc = "git [p]review hunk";
-        };
-      }
-      { # official gitsigns
-        mode = "n";
-        key = "<leader>hi";
-        action.__raw = ''
-          function()
-            require('gitsigns').preview_hunk_inline()
-          end
-        '';
-        options = {
-          desc = "git preview hunk [i]nline";
-        };
-      }
-      {
-        mode = "n";
-        key = "<leader>hb";
-        action.__raw = ''
+      (mapP  [ "<leader>hs"  "<cmd>Gitsigns stage_hunk<CR>"       "[s]tage hunk"       ])
+      (mapP  [ "<leader>hr"  "<cmd>Gitsigns reset_hunk<CR>"       "[r]eset hunk"       ])
+      (mapP  [ "<leader>hS"  "<cmd>Gitsigns stage_buffer<CR>"     "[S]tage buffer"     ])
+      (mapP  [ "<leader>hu"  "<cmd>Gitsigns undo_stage_hunk<CR>"  "[u]ndo stage hunk"  ]) # deprecated, use stage_hunk which also toggles
+      (mapP  [ "<leader>hR"  "<cmd>Gitsigns reset_buffer<CR>"     "[R]eset buffer"     ])
+      (mapP  [ "<leader>hp"  "<cmd>Gitsigns preview_hunk<CR>"     "[p]review hunk"     ])
+
+      # official gitsigns
+      (mapP  [ "<leader>hi"  "<cmd>Gitsigns preview_hunk_inline<CR>"  "preview hunk [i]nline"  ])
+      (mapPR [ "<leader>hb"  ''
           function()
             require('gitsigns').blame_line({ full = true })
           end
-        '';
-        options = {
-          desc = "git [b]lame line";
-        };
-      }
-      {
-        mode = "n";
-        key = "<leader>hd";
-        action.__raw = ''
-          function()
-            require('gitsigns').diffthis()
-          end
-        '';
-        options = {
-          desc = "git [d]iff against index";
-        };
-      }
-      {
-        mode = "n";
-        key = "<leader>hD";
-        action.__raw = ''
+        '' "[b]lame line" ])
+      (mapP  [ "<leader>hd"  "<cmd>Gitsigns diffthis<CR>"       "[d]iff against index"        ])
+      (mapPR [ "<leader>hD"  ''
           function()
             require('gitsigns').diffthis '@'
           end
-        '';
-        options = {
-          desc = "git [D]iff against last commit";
-        };
-      }
-      { # official gitsigns
-        mode = "n";
-        key = "<leader>hQ";
-        action.__raw = ''
-          function()
-            require('gitsigns').setqflist('all')
-          end
-        '';
-        options = {
-          desc = "git [Q]uickfix List (all)";
-        };
-      }
-      { # official gitsigns
-        mode = "n";
-        key = "<leader>hq";
-        action.__raw = ''
-          function()
-            require('gitsigns').setqflist()
-          end
-        '';
-        options = {
-          desc = "git [q]uickfix List";
-        };
-      }
-      { # auipga
-        mode = "n";
-        key = "<leader>hL";
-        action.__raw = ''
-          function()
-            require('gitsigns').setloclist('all')
-          end
-        '';
-        options = {
-          desc = "git [L]ocation List (all)";
-        };
-      }
-      { # auipga
-        mode = "n";
-        key = "<leader>hl";
-        action.__raw = ''
-          function()
-            require('gitsigns').setloclist()
-          end
-        '';
-        options = {
-          desc = "git [l]ocation List";
-        };
-      }
+        ''  "[D]iff against last commit"  ])
+      (mapP  [ "<leader>hQ"  "<cmd>Gitsigns setqflist all<CR>"  "[Q]uickfix List (all)"       ])
+      (mapP  [ "<leader>hq"  "<cmd>Gitsigns setqflist<CR>"      "[q]uickfix List"             ])
+
+      # auipga
+      (mapP  [ "<leader>hL"  "<cmd>Gitsigns setloclist all<CR>"  "[L]ocation List (all)"  ])
+      (mapP  [ "<leader>hl"  "<cmd>Gitsigns setloclist<CR>"      "[l]ocation List"        ])
+
       # Toggles
-      {
-        mode = "n";
-        key = "<leader>tb";
-        action.__raw = ''
-          function()
-            require('gitsigns').toggle_current_line_blame()
-          end
-        '';
-        options = {
-          desc = "[T]oggle git show [b]lame line";
-        };
-      }
-      {
-        mode = "n";
-        key = "<leader>td";
-        # `toggle_deleted` is deprecated. See https://github.com/nvim-lua/kickstart.nvim/issues/1319
-        #  Replacement `preview_hunk_inline` was accidentally merged. See https://github.com/nvim-lua/kickstart.nvim/pull/1321#issuecomment-2664265962
-        action.__raw = ''
-          function()
-            require('gitsigns').toggle_deleted()
-          end
-        '';
-        options = {
-          desc = "[T]oggle git show [d]eleted";
-        };
-      }
-      { # official gitsigns
-        mode = "n";
-        key = "<leader>tw";
-        action.__raw = ''
-          function()
-            require('gitsigns').toggle_word_diff()
-          end
-        '';
-        options = {
-          desc = "[T]oggle intra-line [w]ord-diff";
-        };
-      }
+      (map [ "<leader>tb"  "<cmd>Gitsigns toggle_current_line_blame<CR>"  "[T]oggle git show [b]lame line"   ])
+      (map [ "<leader>td"  "<cmd>Gitsigns toggle_deleted<CR>"             "[T]oggle git show [d]eleted"      ]) # deprecated
+      (map [ "<leader>tw"  "<cmd>Gitsigns toggle_word_diff<CR>"           "[T]oggle intra-line [w]ord-diff"  ])
+
       # Text object
-      { # official gitsigns
-        mode = [ "o" "x" ];
-        key = "<leader>hh";
-        # key = "<leader>ih"; # official gitsigns
-        action.__raw = ''
-          function()
-            require('gitsigns').select_hunk()
-          end
-        '';
-        options = {
-          desc = "Select hunks as a text object";
-        };
-      }
+      # official gitsigns
+      (map [ "<leader>hh"  "<cmd>Gitsigns select_hunk<CR>"  "Select hunks as a text object"  [ "o" "x" ]  ]) # changed from: <leader>ih
     ];
   };
 }
