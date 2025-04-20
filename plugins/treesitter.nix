e5875f51762f9ec4bd0b9a060a87d4997c68e864
auipga <auipga@gmail.com>
2025-04-20 17:26:11 +0200

    enable additional_vim_regex_highlighting only for Ruby

--- plugins/treesitter.nix
+++ plugins/treesitter.nix
@@ -79,8 +79,10 @@
         highlight = {
           enable = true;
 
-          # Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-          additional_vim_regex_highlighting = true;
+          # Some languages depend on vim's regex highlighting system for indent rules.
+          additional_vim_regex_highlighting = [
+            "ruby"
+          ];
         };
 
         # see `:help nvim-treesitter-incremental-selection-mod`
