938ab169055c4a92b32338a3faaaa0040cffb07a
auipga <auipga@gmail.com>
2025-05-04 21:22:21 +0200

    auto-session: add suggested setup (vim.o.sessionoptions)

--- plugins/custom/plugins/auto-session.nix
+++ plugins/custom/plugins/auto-session.nix
@@ -2,6 +2,13 @@
   programs.nixvim = {
     plugins.auto-session.enable = true;
     plugins.auto-session = {
+      luaConfig.pre = ''
+        -- Recommended sessionoptions config
+        -- https://github.com/rmagatti/auto-session/#recommended-sessionoptions-config
+        vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+        -- which is equivalent to (VimL):
+        -- set sessionoptions+=winpos,terminal,folds
+      '';
       settings = {
       };
     };
