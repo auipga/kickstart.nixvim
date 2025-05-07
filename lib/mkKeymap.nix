a92b8bec5378af584d4cfbadd6af4bef7db691d1
auipga <auipga@gmail.com>
2025-05-07 19:36:11 +0200

    mkKeymap: add parameter for extraOptions

--- lib/mkKeymap.nix
+++ lib/mkKeymap.nix
@@ -1,5 +1,5 @@
 # lib/mkKeymap.nix
-{ prefix ? "", raw ? false }:
+{ prefix ? "", raw ? false, extraOpts ? {} }:
 
 args:
 let
@@ -7,7 +7,7 @@ let
   action = builtins.elemAt args 1;
   desc = if builtins.length args > 2 then builtins.elemAt args 2 else null;
   mode = if builtins.length args > 3 then builtins.elemAt args 3 else "n";
-  extraOptions = if builtins.length args > 4 then builtins.elemAt args 4 else {};
+  extraOptions = if builtins.length args > 4 then builtins.elemAt args 4 else extraOpts;
 
   formattedDesc =
     if desc == null then null else prefix + desc;
