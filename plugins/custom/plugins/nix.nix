dc76e02ad6a6c08e7f7317578cc0795d5f2e4af6
auipga <auipga@gmail.com>
2025-05-17 15:21:39 +0200

    nix: install hmts plugin, treesitter queries for home-manager nix files

--- plugins/custom/plugins/nix.nix
+++ plugins/custom/plugins/nix.nix
@@ -28,5 +28,10 @@
         rootPatterns = [ "flake.nix" ];
       };
     };
+
+    # Custom treesitter queries for Home Manager nix files
+    # https://github.com/calops/hmts.nvim
+    # https://nix-community.github.io/nixvim/plugins/hmts/index.html
+    plugins.hmts.enable = true;
   };
 }
