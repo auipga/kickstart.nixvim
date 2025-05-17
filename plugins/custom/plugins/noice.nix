aad00a5a74644574bc5576eb8ef636d843b75e54
auipga <auipga@gmail.com>
2025-05-17 14:58:56 +0200

    noice: reset to defaults, add comments

--- plugins/custom/plugins/noice.nix
+++ plugins/custom/plugins/noice.nix
@@ -7,11 +7,11 @@
     plugins.noice.settings = {
       # Suggested setup
       presets = {
-        bottom_search = true; # default: false;
-        command_palette = true; # default: false;
-        inc_rename = true; # default: false;
-        long_message_to_split = true; # default: false;
-        lsp_doc_border = true; # default: false;
+        bottom_search = true; # use a classic bottom cmdline for search
+        command_palette = true; # position the cmdline and popupmenu together
+        long_message_to_split = true; # long messages will be sent to a split
+        inc_rename = false; # enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false; # add a border to hover docs and signature help
       };
     };
   };
