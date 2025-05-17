faaab8db92606a713297ae8c7c6a74f5ba4967ec
auipga <auipga@gmail.com>
2025-05-17 15:05:08 +0200

    conform: disable format_on_save

--- plugins/conform.nix
+++ plugins/conform.nix
@@ -18,18 +18,19 @@ in
       enable = true;
       settings = {
         notify_on_error = false;
-        format_on_save = ''
-          function(bufnr)
-            -- Disable "format_on_save lsp_fallback" for lanuages that don't
-            -- have a well standardized coding style. You can add additional
-            -- lanuages here or re-enable it for the disabled ones.
-            local disable_filetypes = { c = true, cpp = true }
-            return {
-              timeout_ms = 500,
-              lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype]
-            }
-          end
-        '';
+        # -> disable auto-format to not screw up my nice nix files
+        # format_on_save = ''
+        #   function(bufnr)
+        #     -- Disable "format_on_save lsp_fallback" for lanuages that don't
+        #     -- have a well standardized coding style. You can add additional
+        #     -- lanuages here or re-enable it for the disabled ones.
+        #     local disable_filetypes = { c = true, cpp = true }
+        #     return {
+        #       timeout_ms = 500,
+        #       lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype]
+        #     }
+        #   end
+        # '';
         formatters_by_ft = {
           lua = ["stylua"];
           # Conform can also run multiple formatters sequentially
