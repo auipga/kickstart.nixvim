d130a90041cc24237db238a94a53510d1c3eda3e
auipga <auipga@gmail.com>
2025-06-09 22:54:22 +0200

    toggleterm: add Ctrl+T

--- plugins/custom/plugins/terminal.nix
+++ plugins/custom/plugins/terminal.nix
@@ -3,7 +3,7 @@
     plugins.toggleterm.enable = true;
     plugins.toggleterm = {
       settings = {
-        open_mapping = "[[<c-\\>]]";
+        open_mapping = "{ [[<C-t>]], [[<C-\\>]] }"; # default: "[[<c-\\>]]";
       };
     };
   };
