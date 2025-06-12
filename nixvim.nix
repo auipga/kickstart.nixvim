a0ca1f70a4cb44491210a53911935018d3feb06b
auipga <auipga@gmail.com>
2025-06-12 23:42:22 +0200

    better comments

--- nixvim.nix
+++ nixvim.nix
@@ -135,10 +135,9 @@ in
 
       # if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
       # instead raise a dialog asking if you wish to save the current file(s)
-      # See `:help 'confirm'`
       confirm = true;
 
-      # See `:help hlsearch`
+      # Highlight search matches
       hlsearch = true;
 
       # Show upto 9 columns of folds
