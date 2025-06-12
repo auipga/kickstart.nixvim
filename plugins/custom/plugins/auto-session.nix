018bfe383b0c7f3751723c71263bc267db5ed217
auipga <auipga@gmail.com>
2025-06-12 23:52:54 +0200

    auto-session: prepare more configuration

--- plugins/custom/plugins/auto-session.nix
+++ plugins/custom/plugins/auto-session.nix
@@ -21,6 +21,23 @@ in
         ];
 
         use_git_branch = true;
+
+        # On startup, loads the last saved session if session for cwd does not exist
+        # auto_restore_last_session = true; # default: false;
+        # Include git branch name in session name
+        # git_use_branch_name = true; # default: false;
+        # Should we auto-restore the session when the git branch changes. Requires git_use_branch_name
+        # git_auto_restore_on_branch_change = true; # default: false;
+        # Whether to show a notification when auto-restoring
+        # show_auto_restore_notif = true; # default: false;
+        # Follow cwd changes, saving a session before change and restoring after
+        # cwd_change_handling = true; # default: false;
+        # Should language servers be stopped when restoring a session. Can also be a function that will be called if set. Not called on autorestore from startup
+        # lsp_stop_on_restore = true; # default: false;
+        # Called when there's an error restoring. By default, it ignores fold errors otherwise it displays the error and returns false to disable auto_save
+        # restore_error_handler = null; # default: null;
+        # Sets the log level of the plugin (debug, info, warn, error).
+        # log_level = "error"; default: "error";
       };
     };
 
