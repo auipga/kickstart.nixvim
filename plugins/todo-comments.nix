9ba161d56018f98b911ff5dd8d7048bc07a11c37
auipga <auipga@gmail.com>
2025-06-12 23:45:40 +0200

    todo-comments: prepare keymaps

--- plugins/todo-comments.nix
+++ plugins/todo-comments.nix
@@ -1,3 +1,6 @@
+#let
+#  map = import ../lib/mkKeymap.nix { };
+#in
 {
   programs.nixvim = {
     # Highlight todo, notes, etc in comments
@@ -7,5 +10,13 @@
       settings = {
       };
     };
+
+#    keymaps = [
+# TODO: this will override t = tnext/tprevious, T = tlast/trewind
+#      (map [ "]t"  "<cmd>lua require('todo-comments').jump_next()<CR>"  "Next todo comment" ])
+#      (map [ "[t"  "<cmd>lua require('todo-comments').jump_prev()<CR>"  "Previous todo comment" ])
+#      (map [ "]T"  "<cmd>lua require('todo-comments').jump_next({keywords = { \"ERROR\", \"WARNING\" }})<CR>"  "Next error/warning comment" ])
+#      (map [ "[T"  "<cmd>lua require('todo-comments').jump_prev({keywords = { \"ERROR\", \"WARNING\" }})<CR>"  "Previous error/warning comment" ])
+#    ];
   };
 }
