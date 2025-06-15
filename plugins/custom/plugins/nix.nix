d14312827d06d170190bfc01965919c8adeec414
auipga <auipga@gmail.com>
2025-06-15 13:37:42 +0200

    prepare autocomplete for `programs.`

--- plugins/custom/plugins/nix.nix
+++ plugins/custom/plugins/nix.nix
@@ -24,6 +24,11 @@
       settings = {
         nil = {
           formatting.command = [ "nixfmt" ]; # default: "nixpkgs-fmt"
+#          nixpkgs = {
+#            # Ensures Nixpkgs schema support (autocomplete for `programs.` and others)
+#            useRegistry = true;
+#            registry = { nixpkgs = "nixpkgs"; };
+#          };
         };
         rootPatterns = [ "flake.nix" ];
       };
