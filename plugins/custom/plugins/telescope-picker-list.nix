7c9d3396a749f5b437afee26936f0a28310ae237
auipga <auipga@gmail.com>
2025-06-15 13:35:24 +0200

    prepare picker-specific options

--- plugins/custom/plugins/telescope-picker-list.nix
+++ plugins/custom/plugins/telescope-picker-list.nix
@@ -34,6 +34,14 @@ in
       settings = {
         extensions = {
           picker_list = {
+            # picker-specific options
+            opts = {
+#              project = { display_type = "full"; };
+#              emoji.__raw   = "require('telescope.themes').get_dropdown({})";
+#              luasnip.__raw = "require('telescope.themes').get_dropdown({})";
+#              notify.__raw  = "require('telescope.themes').get_dropdown({})";
+            };
+
             excluded_pickers = [
               # suggested
               "fzf"
