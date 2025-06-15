9a3ee10c61ce267871bf3a46aabfe0e340106f39
auipga <auipga@gmail.com>
2025-06-15 13:48:35 +0200

    AI (history): restore some default config

--- plugins/custom/plugins/ai-codecompanion-history.nix
+++ plugins/custom/plugins/ai-codecompanion-history.nix
@@ -23,15 +23,15 @@ in
       # Keymap to open history from chat buffer
       keymap = "gh";
       # Automatically generate titles for new chats
-      auto_generate_title = false; # default: true;
+      auto_generate_title = true; # default: true;
       # On exiting and entering neovim, loads the last chat on opening chat
       continue_last_chat = true; # default: false;
       # When chat is cleared with `gx` delete the chat from history
       delete_on_clearing_chat = false;
       # Picker interface ("telescope"; "snacks" or "default")
-      picker = "telescope";
+      picker = "snacks";
       # Enable detailed logging for history extension
-      enable_logging = true; # default: false;
+      enable_logging = false;
       # Directory path to save the chats
       dir_to_save.__raw = "vim.fn.stdpath('data') .. '/codecompanion-history'";
     };
