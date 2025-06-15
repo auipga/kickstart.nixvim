a96bc26017d2071c1e49f4924f78af0e5af96bb4
auipga <auipga@gmail.com>
2025-06-15 15:31:46 +0200

    TODO: checkout plugins

--- plugins/custom/plugins/testing.nix
+++ plugins/custom/plugins/testing.nix
@@ -1,5 +1,18 @@
 {
   programs.nixvim = {
     # plugins.___.enable = true;
+    /*
+      dashboard
+      lsp-*
+
+      https://nix-community.github.io/nixvim/plugins/copilot-chat/index.html
+      Copilot Chat, cmp, lua, vim
+    */
+
+    plugins.vim-be-good.enable = true;
+    #    plugins.alpha = {
+    #      enable = true;
+    #      theme = "dashboard";
+    #    };
   };
 }
