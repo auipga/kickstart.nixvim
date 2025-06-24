2593b72f5b688e02a65397e77f968cca61f84034
auipga <auipga@gmail.com>
2025-06-24 16:02:58 +0200

    terminal: remove unused mapping

--- plugins/custom/plugins/terminal.nix
+++ plugins/custom/plugins/terminal.nix
@@ -6,7 +6,7 @@ in
     plugins.toggleterm.enable = true;
     plugins.toggleterm = {
       settings = {
-        open_mapping = "{ [[<C-t>]], [[<C-\\>]] }"; # default: "[[<c-\\>]]";
+        open_mapping = "{ [[<C-t>]] }"; # default: "[[<c-\\>]]";
       };
     };
 
