9be14b65765abe498f5e74c79010287ab2c6edce
auipga <auipga@gmail.com>
2025-06-27 19:37:05 +0200

    vectorcode-nvim: prepare for openvino/intel

--- plugins/custom/plugins/ai-codecompanion-vectorcode.nix
+++ plugins/custom/plugins/ai-codecompanion-vectorcode.nix
@@ -90,14 +90,23 @@ let
   };
 
   # configure the program
-#  home.file.".vectorcode/config.json".text = ''
-#    {
-#      "embedding_params": {
-#        "backend": "torch",
-#        "device": "cuda"
-#      },
-#    }
-#  '';
+  # for intel:
+  # home.file.".vectorcode/config.json".text = ''
+  #   {
+  #     "embedding_params": {
+  #       "backend": "openvino"
+  #     }
+  #   }
+  # '';
+  # for nvidia:
+  # home.file.".vectorcode/config.json".text = ''
+  #   {
+  #     "embedding_params": {
+  #       "backend": "torch",
+  #       "device": "cuda"
+  #     }
+  #   }
+  # '';
 
   # zsh completions
   xdg.configFile."zsh/completions/_vectorcode".source = "${vectorcodeCompletion}/_vectorcode";
