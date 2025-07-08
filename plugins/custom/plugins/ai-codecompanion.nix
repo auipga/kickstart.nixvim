a5647b55610278d7970c7bb4d2e296e28658fdca
auipga <auipga@gmail.com>
2025-07-08 18:54:46 +0200

    ai/codecompanion: re-implement default prompt_library, prepare for customization

--- plugins/custom/plugins/ai-codecompanion.nix
+++ plugins/custom/plugins/ai-codecompanion.nix
@@ -85,7 +85,7 @@ in
           provider = if config.programs.nixvim.plugins.snacks.enable then "snacks" else "default";
           opts = {
             show_default_actions = true; # Show the default actions in the action palette?
-            show_default_prompt_library = true; # Show the default prompt library in the action palette?
+            show_default_prompt_library = false; # Show the default prompt library in the action palette?
           };
         };
         chat = {
@@ -179,9 +179,7 @@ When given a task:
         };
       };
 
-      # prompt_library = {
-      #   # see https://codecompanion.olimorris.dev/configuration/prompt-library.html
-      # };
+      prompt_library = import ./ai-codecompanion-prompts.nix;
     };
 
     keymaps = [
