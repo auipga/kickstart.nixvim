59d79a082dacad1aa455af4de108f76ee01e3d69
auipga <auipga@gmail.com>
2025-07-09 12:51:24 +0200

    ai/codecompanion/vectorcode: remove opts that are the default anyways

--- plugins/custom/plugins/ai-codecompanion-vectorcode.nix
+++ plugins/custom/plugins/ai-codecompanion-vectorcode.nix
@@ -8,19 +8,6 @@
       extensions.vectorcode = {
         enabled = true;
         opts = {
-          add_tool = true; # the @vectorcode tool becomes available in the CodeCompanion chat buffer
-          add_slash_command = true; # the /vectorcode slash command
-          tool_group = {
-            enabled = true;
-            collapse = true;
-            # tools in this array will be included to the `vectorcode_toolbox` tool group
-            extras = {};
-          };
-          tool_opts = {
-            ls = {};
-            query = {};
-            vectorise = {};
-          };
         };
       };
 
