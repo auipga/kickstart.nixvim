a0b6f0423e6924f1172c4111f6c8e26fd38e4b85
auipga <auipga@gmail.com>
2025-07-24 18:57:47 +0200

    update todos

--- plugins/custom/plugins/testing.nix
+++ plugins/custom/plugins/testing.nix
@@ -2,16 +2,18 @@
   programs.nixvim = {
     # plugins.___.enable = true;
     /*
-      dashboard
       lsp-*
 
       https://nix-community.github.io/nixvim/plugins/copilot-chat/index.html
       Copilot Chat, cmp, lua, vim
-    */
 
-    #    plugins.alpha = {
-    #      enable = true;
-    #      theme = "dashboard";
-    #    };
+      Inc/Dec (C-A/I) for dates:
+      plugins.speeddating.enable = true;
+      # TODO: request this for nixvim
+
+      vim-abolish https://github.com/tpope/vim-abolish
+        :%Subvert/facilit{y,ies}/building{,s}/g
+
+    */
   };
 }
