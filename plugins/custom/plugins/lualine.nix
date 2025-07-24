773b5e7d6b34f9b8b49e18e7fc44c01afc959ee5
auipga <auipga@gmail.com>
2025-07-24 17:29:46 +0200

    lualine: switch _x to lua code, prepare

--- plugins/custom/plugins/lualine.nix
+++ plugins/custom/plugins/lualine.nix
@@ -37,11 +37,13 @@
           "diagnostics"
         ];
         lualine_c = [ "filename" ];
-        lualine_x = [
-          "encoding"
-          "fileformat"
-          "filetype"
-        ];
+        lualine_x.__raw = ''
+          {
+            "encoding",
+            "fileformat",
+            "filetype",
+          }
+        '';
         lualine_y = [ "progress" ];
         lualine_z = [ "location" ];
       };
