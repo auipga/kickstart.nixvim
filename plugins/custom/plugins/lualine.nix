a963ef39ab216ded019014ef1fb560510a0adcdf
auipga <auipga@gmail.com>
2025-07-24 17:27:14 +0200

    lualine: add default sections

--- plugins/custom/plugins/lualine.nix
+++ plugins/custom/plugins/lualine.nix
@@ -30,6 +30,20 @@
         # "trouble"         # see ./trouble.nix
       ];
       sections = {
+        lualine_a = [ "mode" ];
+        lualine_b = [
+          "branch"
+          "diff"
+          "diagnostics"
+        ];
+        lualine_c = [ "filename" ];
+        lualine_x = [
+          "encoding"
+          "fileformat"
+          "filetype"
+        ];
+        lualine_y = [ "progress" ];
+        lualine_z = [ "location" ];
       };
     };
   };
