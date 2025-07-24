2428e73d2eb8f5946f596a7f911d41b57d9e16c2
auipga <auipga@gmail.com>
2025-07-24 16:58:06 +0200

    which-key: remove fallback fake-icons

--- plugins/which-key.nix
+++ plugins/which-key.nix
@@ -12,47 +12,6 @@
         # Expand groups when <= n mappings.
         expand = 0;
 
-        icons = lib.mkMerge [
-          (if config.programs.nixvim.globals.have_nerd_font then
-          # don’t override keys, fallback to default
-            { }
-          else {
-            keys = {
-              Up = "<Up> ";
-              Down = "<Down> ";
-              Left = "<Left> ";
-              Right = "<Right> ";
-              C = "<C-…> ";
-              M = "<M-…> ";
-              D = "<D-…> ";
-              S = "<S-…> ";
-              CR = "<CR> ";
-              Esc = "<Esc> ";
-              ScrollWheelDown = "<ScrollWheelDown> ";
-              ScrollWheelUp = "<ScrollWheelUp> ";
-              NL = "<NL> ";
-              BS = "<BS> ";
-              Space = "<Space> ";
-              Tab = "<Tab> ";
-              F1 = "<F1>";
-              F2 = "<F2>";
-              F3 = "<F3>";
-              F4 = "<F4>";
-              F5 = "<F5>";
-              F6 = "<F6>";
-              F7 = "<F7>";
-              F8 = "<F8>";
-              F9 = "<F9>";
-              F10 = "<F10>";
-              F11 = "<F11>";
-              F12 = "<F12>";
-            };
-          })
-          {
-            # space to merge other keys below settings.icons
-          }
-        ];
-
         preset = "modern"; # bordered, bottom, wide, multi column
         # preset = "helix"; # bordered, bottom right, single column
 
