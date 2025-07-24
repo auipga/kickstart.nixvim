4743ef2b9669d37e182939b5fba582d72e294888
auipga <auipga@gmail.com>
2025-07-24 17:26:50 +0200

    lualine: temp: remove vectorcode integration

--- plugins/custom/plugins/lualine.nix
+++ plugins/custom/plugins/lualine.nix
@@ -30,14 +30,6 @@
         # "trouble"         # see ./trouble.nix
       ];
       sections = {
-        lualine_y.__raw = ''
-          {
-            -- see https://github.com/Davidyz/VectorCode/blob/main/docs/neovim/README.md#status-line-component
-            require("vectorcode.integrations").lualine({
-              show_job_count = true, -- default: false
-            })
-          }
-        '';
       };
     };
   };
