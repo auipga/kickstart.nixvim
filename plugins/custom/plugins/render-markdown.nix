1735fdec7fe38866d41504279005cd9cf3e29512
auipga <auipga@gmail.com>
2025-08-19 20:11:13 +0200

    render-markdown: disable latex = silence warning 'parser not installed'

--- plugins/custom/plugins/render-markdown.nix
+++ plugins/custom/plugins/render-markdown.nix
@@ -3,5 +3,9 @@
     # https://github.com/MeanderingProgrammer/render-markdown.nvim/
     # https://nix-community.github.io/nixvim/plugins/render-markdown/index.html
     plugins.render-markdown.enable = true;
+    plugins.render-markdown.settings = {
+      # silence warning 'parser not installed'
+      latex.enabled = false;
+    };
   };
 }
