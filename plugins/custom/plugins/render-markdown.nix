6f0dc572b0b95c533c3d246d3f55cc87f5e2a011
auipga <auipga@gmail.com>
2025-08-19 20:13:20 +0200

    codecompanion/render-markdown: use render-markdown for codecompanion filetype (chat)

--- plugins/custom/plugins/render-markdown.nix
+++ plugins/custom/plugins/render-markdown.nix
@@ -4,6 +4,13 @@
     # https://nix-community.github.io/nixvim/plugins/render-markdown/index.html
     plugins.render-markdown.enable = true;
     plugins.render-markdown.settings = {
+      file_types = [
+        "markdown" # default
+        "codecompanion"
+        # TODO: use lib.mkAfter in ./ai-codecompanion.nix
+        # right now this won't work as it 'replaces' it and 'needs' lib.mkDefault and mkForce
+      ];
+
       # silence warning 'parser not installed'
       latex.enabled = false;
     };
