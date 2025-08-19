b03244a25a589d7d21b9559a8e8e6a16a306546d
auipga <auipga@gmail.com>
2025-08-19 21:37:45 +0200

    ai: prepare for copilot

--- plugins/custom/plugins/ai-copilot.nix
+++ plugins/custom/plugins/ai-copilot.nix
@@ -1,12 +1,34 @@
 {
   programs.nixvim = {
-#    plugins.copilot-lua.enable = true; # preferable over official copilot-vim
-#
-#    plugins.copilot-cmp.enable = true; # 22 KiB
-#    plugins.copilot-chat.enable = true; # 367 KiB
-#
-#    plugins.blink-cmp.enable = true; # 5.1 MiB
-#    plugins.blink-cmp-copilot.enable = true; # 88.7 KiB
-#    plugins.blink-copilot.enable = true; # 24.0 KiB
+    plugins.copilot-lua.enable = !true; # preferable over official copilot-vim
+    # setup:
+    # :Copilot auth
+    #
+    plugins.copilot-lua.settings = {
+      filetypes = {
+        nix = true;
+        sh.__raw = ''
+          function()
+            if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), '^%.env.*') then
+              -- disable for .env files
+              return false
+            end
+            return true
+          end
+        '';
+
+      };
+    };
+
+    # plugins.copilot-cmp.enable = true; # 22 KiB
+
+    # https://github.com/Davidyz/VectorCode/wiki/Neovim-Integrations#copilotc-nvimcopilotchatnvim
+    # plugins.copilot-chat.enable = true; # 367 KiB
+
+    # plugins.blink-cmp.enable = true; # 5.1 MiB
+
+    # plugins.blink-cmp-copilot.enable = true; # 88.7 KiB
+
+    # plugins.blink-copilot.enable = true; # 24.0 KiB
   };
 }
