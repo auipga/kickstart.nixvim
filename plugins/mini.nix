9ba652690aefdeb12537f5680c6c7ff515ff25da
auipga <auipga@gmail.com>
2025-09-30 18:06:50 +0200

    mini: update links to docs

--- plugins/mini.nix
+++ plugins/mini.nix
@@ -1,6 +1,7 @@
 {
   programs.nixvim = {
     # Collection of various small independent plugins/modules
+    # https://github.com/nvim-mini/mini.nvim/
     # https://nix-community.github.io/nixvim/plugins/mini/index.html
     plugins.mini = {
       enable = true;
@@ -8,7 +9,7 @@
       # https://nix-community.github.io/nixvim/plugins/mini/index.html#pluginsminimodules
       modules = {
         # Better Around/Inside textobjects
-        # https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-ai.md
+        # https://github.com/nvim-mini/mini.nvim/blob/main/readmes/mini-ai.md
         # Examples:
         #  - va)  - [V]isually select [A]round [)]paren
         #  - yinq - [Y]ank [I]nside [N]ext [Q]uote
@@ -19,7 +20,7 @@
         };
 
         # Align text interactively
-        # https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-align.md
+        # https://github.com/nvim-mini/mini.nvim/blob/main/readmes/mini-align.md
         align = {
           # :h MiniAlign-modifiers-builtin
           # :h MiniAlign-examples
@@ -27,12 +28,12 @@
 
         # Tweak and save any color scheme
         # (also installs colorschemes mini[spring|summer|autumn|winter], minicyan, minischeme, randomhue)
-        # https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-colors.md
+        # https://github.com/nvim-mini/mini.nvim/blob/main/readmes/mini-colors.md
         # colors = {}
         # Run with :lua require('mini.colors').interactive()
 
         # Comment lines
-        # https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-comment.md
+        # https://github.com/nvim-mini/mini.nvim/blob/main/readmes/mini-comment.md
         # comment = {
         #   mappings = {
         #     comment = "<leader>/";
@@ -44,13 +45,13 @@
         # INFO: I use the built-in commenting for now (:h commenting)
 
         # Automatic highlighting of word under cursor
-        # https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-cursorword.md
+        # https://github.com/nvim-mini/mini.nvim/blob/main/readmes/mini-cursorword.md
         cursorword = {
           delay = 100;
         };
 
         # Highlight patterns in text
-        # https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-hipatterns.md
+        # https://github.com/nvim-mini/mini.nvim/blob/main/readmes/mini-hipatterns.md
         hipatterns = {
           highlighters = {
             # Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
@@ -66,42 +67,42 @@
           };
         };
 
-        # https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-icons.md#demo
+        # https://github.com/nvim-mini/mini.nvim/blob/main/readmes/mini-icons.md#demo
         icons = {};
 
         # Extend f, F, t, T to work on multiple lines + repeat + highlight + dot
-        # https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-jump.md
+        # https://github.com/nvim-mini/mini.nvim/blob/main/readmes/mini-jump.md
         jump = {};
 
         # Jump to exact location on screen using abc
-        # https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-jump2d.md
+        # https://github.com/nvim-mini/mini.nvim/blob/main/readmes/mini-jump2d.md
         jump2d = {
           view.dim = true;
           allowed_windows.not_current = false;
         };
 
         # Move any selection in any direction
-        # https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-move.md
+        # https://github.com/nvim-mini/mini.nvim/blob/main/readmes/mini-move.md
         move = {
           # Default keymaps: Alt (Meta) + hjkl
         };
 
         # Minimal and fast autopairs
-        # https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-pairs.md
+        # https://github.com/nvim-mini/mini.nvim/blob/main/readmes/mini-pairs.md
         # pairs = {};
         # INFO: replaced by ../plugins/kickstart/plugins/autopairs.nix
 
         # Pick anything
-        # https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-pick.md
+        # https://github.com/nvim-mini/mini.nvim/blob/main/readmes/mini-pick.md
         pick = {};
 
         # Manage and expand snippets
-        # https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-snippets.md
+        # https://github.com/nvim-mini/mini.nvim/blob/main/readmes/mini-snippets.md
         # TODO: checkout, needed at all?
         # snippets = {};
 
         # Split and join arguments
-        # https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-splitjoin.md
+        # https://github.com/nvim-mini/mini.nvim/blob/main/readmes/mini-splitjoin.md
         # Default keymap: gS to toggle
         splitjoin = {};
         # INFO:
@@ -109,7 +110,7 @@
         # it can expand ? to match Ok/Err :+1:
 
         # Add/delete/replace surroundings (brackets, quotes, etc.)
-        # https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-surround.md
+        # https://github.com/nvim-mini/mini.nvim/blob/main/readmes/mini-surround.md
         #
         # Examples:
         #  - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
@@ -125,14 +126,14 @@
         # Simple and easy statusline.
         #  You could remove this setup call if you don't like it,
         #  and try some other statusline plugin
-        # https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-statusline.md
+        # https://github.com/nvim-mini/mini.nvim/blob/main/readmes/mini-statusline.md
         # statusline = {
         #   use_icons.__raw = "vim.g.have_nerd_font";
         # };
 
         # ... and there is more!
-        # Check out: https://github.com/echasnovski/mini.nvim
-        # https://github.com/echasnovski/mini.nvim/blob/main/README.md
+        # Check out: https://github.com/nvim-mini/mini.nvim
+        # https://github.com/nvim-mini/mini.nvim/blob/main/README.md
       };
     };
 
