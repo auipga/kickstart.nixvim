6212eed33dc31555e56b955d203170d9f53aa4bb
auipga <auipga@gmail.com>
2025-10-06 19:38:34 +0200

    cleanup, add links to docs

--- plugins/custom/plugins/trouble.nix
+++ plugins/custom/plugins/trouble.nix
@@ -1,6 +1,8 @@
 { lib, ... }:
 {
   programs.nixvim = {
+    # https://github.com/folke/trouble.nvim/
+    # https://nix-community.github.io/nixvim/plugins/trouble/index.html
     plugins.trouble.enable = true;
 
     # Integrations
--- plugins/custom/plugins/wakatime.nix
+++ plugins/custom/plugins/wakatime.nix
@@ -1,5 +1,7 @@
 {
   programs.nixvim = {
+    # https://github.com/wakatime/vim-wakatime/
+    # https://nix-community.github.io/nixvim/plugins/wakatime.html
     plugins.wakatime.enable = true;
     # Setup:
     # :WakaTimeApiKey<cr>
