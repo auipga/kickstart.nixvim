b8ecfbaf7738e177e3313c66ff71971371245318
auipga <auipga@gmail.com>
2025-11-23 04:17:15 +0100

    lsp: remove Goto Declaration (gD) keymap

--- plugins/lsp.nix
+++ plugins/lsp.nix
@@ -119,9 +119,6 @@ in
           # Execute a code action, usually your cursor needs to be on top of
           #  an error or a suggestion from your LSP for this to activate.
           [ "<leader>ca"  "code_action"  "LSP: [C]ode [A]ction"  [ "n" "x" ]  ]
-          # WARN: This is not Goto Definition, this is Goto Declaration.
-          #  For example, in C this would take you to the header.
-          [ "gD"          "declaration"  "LSP: [G]oto [D]eclaration"   "n"    ]
         ];
       };
 
