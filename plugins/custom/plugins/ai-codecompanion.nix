3e75478f1cd86937cfa2312744e6b58da3e6ee11
auipga <auipga@gmail.com>
2025-12-28 13:40:12 +0100

    codecompanion: comment out schema default values for openai adapter
    they don't work with gpt-5

--- plugins/custom/plugins/ai-codecompanion.nix
+++ plugins/custom/plugins/ai-codecompanion.nix
@@ -33,9 +33,9 @@ in
               env = { api_key = "cmd:cat $HOME/.config/sops-nix/secrets/api-keys/work/OPENAI" },
               schema = {
                 model       = { default = "gpt-5" },
-                max_tokens  = { default = 2048 },
-                temperature = { default = 0.2 },
-                top_p       = { default = 0.95 },
+                -- max_tokens  = { default = 2048 },
+                -- temperature = { default = 0.2 },
+                -- top_p       = { default = 0.95 },
               },
             })
           end
