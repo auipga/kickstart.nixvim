c7d2d8b59f05d87f87ee9c4d287dc7b99d0935c8
auipga <auipga@gmail.com>
2025-12-30 14:41:06 +0100

    cleanup ideas for testing

--- plugins/custom/plugins/testing.nix
+++ plugins/custom/plugins/testing.nix
@@ -1,15 +1,5 @@
 {
   programs.nixvim = {
     # plugins.___.enable = true;
-    /*
-      lsp-*
-
-      https://nix-community.github.io/nixvim/plugins/copilot-chat/index.html
-      Copilot Chat, cmp, lua, vim
-
-      vim-abolish https://github.com/tpope/vim-abolish
-        :%Subvert/facilit{y,ies}/building{,s}/g
-
-    */
   };
 }
