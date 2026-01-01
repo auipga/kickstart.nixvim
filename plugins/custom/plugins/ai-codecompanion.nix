c3c67356173e8856240a034a20ca7c9798b187bd
auipga <auipga@gmail.com>
2026-01-01 22:50:02 +0100

    codecompanion: create rules.hardware

--- plugins/custom/plugins/ai-codecompanion.nix
+++ plugins/custom/plugins/ai-codecompanion.nix
@@ -224,6 +224,50 @@ in
           ];
           is_preset = true;
         };
+
+        hardware = {
+          description = "[my hardware]";
+          enabled = true;
+          parser = "CodeCompanion";
+          files = {
+            "pc" = {
+              description = "My PC specs";
+              files = [
+               "~/.rules/hardware/default.md"
+               "~/.rules/hardware/pc.md"
+              ];
+            };
+            "pc-next" = {
+              description = "The next PC";
+              files = [
+               "~/.rules/hardware/default.md"
+               "~/.rules/hardware/pc-next.md"
+              ];
+            };
+            "steamdeck" = {
+              description = "My Steam Deck";
+              files = [
+               "~/.rules/hardware/default.md"
+               "~/.rules/hardware/steamdeck.md"
+              ];
+            };
+            "peripherals" = {
+              description = "My peripherals";
+              files = [
+               "~/.rules/hardware/default.md"
+               "~/.rules/hardware/peripherals.md"
+              ];
+            };
+            "spare" = {
+              description = "My spare hardware";
+              files = [
+               "~/.rules/hardware/default.md"
+               "~/.rules/hardware/spare.md"
+              ];
+            };
+          };
+          is_preset = true;
+        };
       };
     };
 
