c6d4d3df7cce7394b06cc1ecbebf7247f6df9e20
auipga <auipga@gmail.com>
2026-01-01 22:52:04 +0100

    codecompanion: create rules.network

--- plugins/custom/plugins/ai-codecompanion.nix
+++ plugins/custom/plugins/ai-codecompanion.nix
@@ -268,6 +268,27 @@ in
           };
           is_preset = true;
         };
+
+        network = {
+          description = "[my network]";
+          enabled = true;
+          parser = "CodeCompanion";
+          files = {
+            "internet" = {
+              description = "My internet";
+              files = [ "~/.rules/network/internet.md" ];
+            };
+            "router" = {
+              description = "My router";
+              files = [ "~/.rules/network/router.local.md" ];
+            };
+            "router-next" = {
+              description = "The next router";
+              files = [ "~/.rules/network/router-next.md" ];
+            };
+          };
+          is_preset = true;
+        };
       };
     };
 
