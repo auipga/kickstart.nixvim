968058ccce0ebadbd90ba2764074998272f2f11d
auipga <auipga@gmail.com>
2026-01-07 20:19:31 +0100

    codecompanion: explicitely set diff split layout (no change, uncomment)

--- plugins/custom/plugins/ai-codecompanion.nix
+++ plugins/custom/plugins/ai-codecompanion.nix
@@ -187,7 +187,7 @@ in
             };
             split = {
               close_chat_at = 200; # default: 240, Close an open chat buffer if the total columns of your display are less than...
-              # layout = "vertical"; # vertical*|horizontal split for default provider
+              layout = "vertical"; # vertical*|horizontal split for default provider
               # opts = [ "internal" "filler" "closeoff" "algorithm:patience" "followwrap" "linematch:120" ];
             };
           };
