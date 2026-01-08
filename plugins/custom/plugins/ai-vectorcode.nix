6443ef6956ac57a2306a9e3b10683da5bf944d26
auipga <auipga@gmail.com>
2026-01-08 13:09:17 +0100

    vectorcode: add options as comment

--- plugins/custom/plugins/ai-vectorcode.nix
+++ plugins/custom/plugins/ai-vectorcode.nix
@@ -45,7 +45,7 @@ let
           query_cb = require("vectorcode.utils").make_surrounding_lines_cb(-1),
           run_on_register = false,
         },
-        async_backend = "default", -- or "lsp"
+        async_backend = "default", -- default|lsp
         exclude_this = true,
         n_query = 1,
         notify = true,
