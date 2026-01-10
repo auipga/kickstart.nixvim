17fb8725e0fe4e4465904bcbdcd2ab134250a5de
auipga <auipga@gmail.com>
2026-01-10 17:31:19 +0100

    lsp: cleanup commented servers

--- plugins/lsp.nix
+++ plugins/lsp.nix
@@ -41,19 +41,8 @@ in
       #  - settings: Override the default settings passed when initializing the server.
       #        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       servers = {
-        # clangd = {
-        #   enable = true;
-        # };
-        # gopls = {
-        #   enable = true;
-        # };
-        # pyright = {
-        #   enable = true;
-        # };
-        # rust_analyzer = {
-        #   enable = true;
-        # };
-        # ...etc. See `https://nix-community.github.io/nixvim/plugins/lsp` for a list of pre-configured LSPs
+        # List of pre-configured LSPs:
+        # https://nix-community.github.io/nixvim/plugins/lsp
         #
         # Some languages (like typescript) have entire language plugins that can be useful:
         #    `https://nix-community.github.io/nixvim/plugins/typescript-tools/index.html`
