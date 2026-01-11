c2bb8fed32e7d0a4e19e5a92ffc2ab258f8c2fe2
auipga <auipga@gmail.com>
2026-01-11 12:57:32 +0100

    cleanup

--- plugins/custom/plugins/codecompanion/extensions/vectorcode.nix
+++ plugins/custom/plugins/codecompanion/extensions/vectorcode.nix
@@ -5,11 +5,7 @@
 
     # Configure the extension
     plugins.codecompanion.settings = {
-      extensions.vectorcode = {
-        enabled = true;
-        opts = {
-        };
-      };
+      extensions.vectorcode.enabled = true;
 
       # Setup: add to context.providers in ./ai-codecompanion.nix
       # { name = "vectorcode"; opts = { top_k = 15; }; }
