ae2e061a56a4ee420fae4a772ddb159682a0bb0d
auipga <auipga@gmail.com>
2026-01-11 14:32:21 +0100

    lazygit: add keymaps

--- plugins/custom/plugins/lazygit.nix
+++ plugins/custom/plugins/lazygit.nix
@@ -1,7 +1,27 @@
+let
+  map = import ../../../lib/mkKeymap.nix { };
+in
 {
   programs.nixvim = {
     # https://github.com/kdheepak/lazygit.nvim
     # https://nix-community.github.io/nixvim/plugins/lazygit/index.html
     plugins.lazygit.enable = true;
+
+    keymaps = [
+      (map [ "<m-g>"       "<cmd>LazyGitCurrentFile<CR>"  "LazyGitCurrentFile"  ])
+
+      (map [ "<leader>gg"  "<cmd>LazyGit<CR>"             "LazyGit"             ])
+      (map [ "<leader>gc"  "<cmd>LazyGitCurrentFile<CR>"  "LazyGitCurrentFile"  ])
+      (map [ "<leader>gC"  "<cmd>LazyGitConfig<CR>"       "LazyGitConfig"       ])
+    ];
+
+    plugins.which-key.settings.spec = [
+      {
+        __unkeyed-1 = "<leader>g";
+        group = "[G]it";
+        mode = [ "n" ];
+        icon = "󰊢";
+      }
+    ];
   };
 }
