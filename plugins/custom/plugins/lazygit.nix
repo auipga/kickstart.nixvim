19c144df6e62fa365ec9317784068170582342fc
auipga <auipga@gmail.com>
2026-02-12 11:56:21 +0100

    lazygit: remove obsolete keymaps

--- plugins/custom/plugins/lazygit.nix
+++ plugins/custom/plugins/lazygit.nix
@@ -9,19 +9,6 @@ in
 
     keymaps = [
       (map [ "<m-g>"       "<cmd>LazyGitCurrentFile<CR>"  "LazyGitCurrentFile"  ])
-
-      (map [ "<leader>gg"  "<cmd>LazyGit<CR>"             "LazyGit"             ])
-      (map [ "<leader>gc"  "<cmd>LazyGitCurrentFile<CR>"  "LazyGitCurrentFile"  ])
-      (map [ "<leader>gC"  "<cmd>LazyGitConfig<CR>"       "LazyGitConfig"       ])
-    ];
-
-    plugins.which-key.settings.spec = [
-      {
-        __unkeyed-1 = "<leader>g";
-        group = "[G]it";
-        mode = [ "n" ];
-        icon = "󰊢";
-      }
     ];
   };
 }
