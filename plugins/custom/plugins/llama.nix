886d5ae37ec10ad90cd75dd1a74f167262840782
auipga <auipga@gmail.com>
2026-02-12 16:38:11 +0100

    llama-vim: automatic host and port based on llama-cpp osConfig

--- plugins/custom/plugins/llama.nix
+++ plugins/custom/plugins/llama.nix
@@ -1,4 +1,8 @@
-{ pkgs, ... }:
+{ osConfig, pkgs, ... }:
+let
+  port = toString osConfig.services.llama-cpp.port; # default: 8012
+  host = if osConfig.services.llama-cpp.openFirewall then "pc" else "127.0.0.1"; # default: "127.0.0.1"
+in
 {
   programs.nixvim = {
     # https://github.com/ggml-org/llama.vim
@@ -6,8 +10,8 @@
 
     extraConfigLua = ''
       vim.g.llama_config = {
-        -- endpoint_fim  = "http://pc:8080/infill",
-        -- endpoint_inst = "http://pc:8080/v1/chat/completions",
+        endpoint_fim  = "http://${host}:${port}/infill", -- default: "http://127.0.0.1:8012/infill"
+        endpoint_inst = "http://${host}:${port}/v1/chat/completions", -- default: "http://127.0.0.1:8012/v1/chat/completions"
       }
     '';
   };
