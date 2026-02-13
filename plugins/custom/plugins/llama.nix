d3693962adc40fda8438a44bde0e4150211264f9
auipga <auipga@gmail.com>
2026-02-13 15:09:02 +0100

    llama-vim: prepare interesting settings

--- plugins/custom/plugins/llama.nix
+++ plugins/custom/plugins/llama.nix
@@ -12,6 +12,11 @@ in
       vim.g.llama_config = {
         endpoint_fim  = "http://${host}:${port}/infill", -- default: "http://127.0.0.1:8012/infill"
         endpoint_inst = "http://${host}:${port}/v1/chat/completions", -- default: "http://127.0.0.1:8012/v1/chat/completions"
+        -- show_info = 2, -- 0|1|2*  0 - disabled, 1 - statusline, 2 - inline
+        -- show_prompt = false, -- default: true
+        -- show_message = true, -- default: true
+        -- auto_fim = false, -- default: true
+        -- enable_at_startup = false, -- default: true
       }
     '';
   };
