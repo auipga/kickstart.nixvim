d4f3b30b062887d0818af8216005f563b4c4886c
auipga <auipga@gmail.com>
2026-02-22 20:06:57 +0100

    wakatime: add setup instructions

--- plugins/wakatime.nix
+++ plugins/wakatime.nix
@@ -4,8 +4,10 @@
     # https://nix-community.github.io/nixvim/plugins/wakatime.html
     plugins.wakatime.enable = true;
     # Setup:
+    # Copy Secret API Key from here:
+    # https://wakatime.com/settings/api-key
+    # and paste it into:
     # :WakaTimeApiKey<cr>
-    # TODO: add key to wakatime.cfg home file via sops
     # https://wakatime.com/settings/account
   };
 }
