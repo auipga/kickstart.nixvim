45cd34ca1d0e04768004233e48d5ee7fb94f960a
auipga <auipga@gmail.com>
2026-05-01 00:00:33 +0200

    cleanup: align comments

--- nixvim.nix
+++ nixvim.nix
@@ -6,9 +6,9 @@ in
   imports = [
     ./custom/terminal.autorun-project-tool.nix
 
-    # ./modules/debug.nix # directly imported by home-manager/modules/coding/languages/rust.nix and php.nix
-    # ./modules/php.nix # directly imported by home-manager/modules/coding/languages/php.nix
-    # ./modules/rust.nix # directly imported by home-manager/modules/coding/languages/rust.nix
+    # ./modules/debug.nix  # directly imported by home-manager/modules/coding/languages/rust.nix and php.nix
+    # ./modules/php.nix    # directly imported by home-manager/modules/coding/languages/php.nix
+    # ./modules/rust.nix   # directly imported by home-manager/modules/coding/languages/rust.nix
     ./modules/performance.nix
     ./modules/testing.nix
 
