e397868f440555a4df5eddb77c586c5a7fb4d3ad
auipga <auipga@gmail.com>
2026-05-01 05:02:22 +0200

    codecompanion/openai: do not force default model

--- plugins/codecompanion/adapters/http/openai.lua
+++ plugins/codecompanion/adapters/http/openai.lua
@@ -3,7 +3,7 @@ function()
   return require("codecompanion.adapters").extend("openai", {
     env = { api_key = "cmd:cat $HOME/.config/sops-nix/secrets/api-keys/work/OPENAI" },
     schema = {
-      model       = { default = "gpt-5" },
+      -- model       = { default = "gpt-5" },
       -- max_tokens  = { default = 2048 },
       -- temperature = { default = 0.2 },
       -- top_p       = { default = 0.95 },
